9f23705c1fce03233b6b4f1d387ad88ff10a4cf4
ZahraaSadeqProd <zkarem12@gmail.com>
2026-03-29 18:36:27 -0700

    Add: longest-common-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    """
+
+    """
+
+    def dfs(self, i, j, rows, cols, text1, text2, cache):
+        if i >= rows or j >= cols:
+            return 0
+
+        if cache[i][j] > 0:
+            return cache[i][j]
+
+        if text1[i] == text2[j]:
+            cache[i][j] = 1 + self.dfs(i + 1, j + 1, rows, cols, text1, text2, cache)
+        else:
+            cache[i][j] = max(self.dfs(i + 1, j, rows, cols, text1, text2, cache),
+                                self.dfs(i, j + 1, rows, cols, text1, text2, cache))
+        return cache[i][j]
+
+
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        rows = len(text1)
+        cols = len(text2)
+        cache = [[0] * cols for i in range(rows)]
+
+        self.dfs(0, 0, rows, cols, text1, text2, cache)
+        print(cache)
+
+        return self.dfs(0, 0, rows, cols, text1, text2, cache)
+
