910295401c395908580c7b8dfd41696ab1303ca9
ZahraaSadeqProd <zkarem12@gmail.com>
2026-05-16 23:29:26 -0700

    Add: replace-elements-with-greatest-element-on-right-side - submission-0

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        
+        nums = [arr[-1]] 
+
+        for i in range(len(arr) - 2, -1, -1):
+            nums.append(max(nums[-1], arr[i]))
+
+        print(nums)
+
+
+        print(nums)
+        nums.reverse()
+
+        nums = nums[1:]
+        nums.append(-1)
+
+
+
+       
+        
+
+        return nums
